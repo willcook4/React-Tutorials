cfaff8558a19194fcc0cece4f838f55b95ffb615
Will Cook <nzwillcook@gmail.com>
2017-01-18 12:20:59 +0000

    Section 3 Lesson 17 complete

--- Udemy/The Complete React Web App Developer Course/HelloReact/public/app.jsx
+++ Udemy/The Complete React Web App Developer Course/HelloReact/public/app.jsx
@@ -1,4 +1,5 @@
-var GreeterMesage = React.createClass({
+// Presentation Component
+var GreeterMessage = React.createClass({
   render: function () {
     var name = this.props.name;
     var message = this.props.message;
@@ -16,19 +17,35 @@ var GreeterForm = React.createClass({
   onFormSubmit: function (e) {
     e.preventDefault();
 
+    var updates = {};
     var name = this.refs.name.value;
+    var message = this.refs.message.value;
 
     if (name.length > 0) {
       this.refs.name.value = '';
-      this.props.onNewName(name);
+      updates.name = name;
     }
+
+    if (message.length > 0) {
+      this.refs.message.value = '';
+      updates.message = message;
+    }
+
+    this.props.onNewData(updates);
   },
   render: function () {
     return (
       <div>
         <form onSubmit={this.onFormSubmit}>
-          <input type="text" ref="name"/>
-          <button>Set Name</button>
+          <div>
+            <input type="text" ref="name" placeholder="Enter name"/>
+          </div>
+          <div>
+            <textarea ref="message" placeholder="Enter message"></textarea>
+          </div>
+          <div>
+            <button>Submit</button>
+          </div>
         </form>
       </div>
     );
@@ -44,22 +61,21 @@ var Greeter = React.createClass({
   },
   getInitialState: function() {
     return {
-      name: this.props.name
+      name: this.props.name,
+      message: this.props.message
     };
   },
-  handleNewName: function(name) {
-    this.setState({
-      name: name
-    });
+  handleNewData: function(updates) {
+    this.setState(updates);
   },
   render: function() {
     var name = this.state.name;
-    var message = this.props.message;
+    var message = this.state.message;
 
     return (
       <div>
-        <GreeterMesage name={name} message={message}/>
-        <GreeterForm onNewName={this.handleNewName}/>
+        <GreeterMessage name={name} message={message}/>
+      <GreeterForm onNewData={this.handleNewData}/>
       </div>
     );
   }
