e2da47c79b488eb17365ec4746016a95a8594f74
Will Cook <nzwillcook@gmail.com>
2017-01-19 12:21:43 +0000

    Section 4 Lecture 25 - React Boiler Plate with ES6

--- Udemy/The Complete React Web App Developer Course/ReactBoilerplate/app/app.jsx
+++ Udemy/The Complete React Web App Developer Course/ReactBoilerplate/app/app.jsx
@@ -1,6 +1,18 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
+var objOne = {
+  name: 'Will',
+  location: 'London'
+};
+
+var objTwo = {
+  age: 28,
+  ...objOne
+};
+
+console.log(objTwo);
+
 ReactDOM.render(
   <h1>Boilerplate App!</h1>,
   document.getElementById('app')
